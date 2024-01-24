f6f0071e0caf61f2e8109fa847bb17f67c100596
Jean-JacquesValmont <jeanjacquesvalmont@gmail.com>
2024-01-24 19:20:45 +0100

    created footer

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-        Footer
+        <div className="bg-black text-center text-white">
+        <p className="">GodsChess 2024, Fabrice et Jean-Jacques Valmont. Tous droits réservés. Mentions légales.</p>
+      </div>
     </footer>
   )
 }
